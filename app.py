a2452a08f8e06292135f6db02296c8314f722676
Afshan Wain <18321519-001@uog.edu.pk>
2022-08-12 19:06:20 +0500

    make it better

--- app.py
+++ app.py
@@ -3,17 +3,30 @@ import cv2 as cv
 import tensorflow as tf
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
+from scipy.ndimage import interpolation as inter
+from PIL import Image as im
+import numpy as np
+from glob import glob
 
 app = Flask(__name__)
-
 
 path = os.getcwd()
 UPLOAD_FOLDER = os.path.join(path, 'uploads\\')
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
+WORDS_UPLOAD_FOLDER = os.path.join(path, 'uploads\\words\\')
+if not os.path.isdir(WORDS_UPLOAD_FOLDER):
+    os.mkdir(WORDS_UPLOAD_FOLDER)
+
+LINES_UPLOAD_FOLDER = os.path.join(path, 'uploads\\lines\\')
+if not os.path.isdir(LINES_UPLOAD_FOLDER):
+    os.mkdir(LINES_UPLOAD_FOLDER)
+
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['LINES_UPLOAD_FOLDER'] = LINES_UPLOAD_FOLDER
+app.config['WORDS_UPLOAD_FOLDER'] = WORDS_UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -37,6 +50,171 @@ def dense_to_text(dense):
     return text
 
 
+# Converting image to a binary image
+def binary_otsus(image, filter: int = 1):
+    # """Binarize an image 0's and 255's using Otsu's Binarization"""
+
+    if len(image.shape) == 3:
+        gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    else:
+        gray_img = image
+
+    # Otsus Binarization
+    if filter != 0:
+        blur = cv.GaussianBlur(gray_img, (3, 3), 0)
+        binary_img = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+    else:
+        binary_img = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+
+    # Morphological Opening
+    # kernel = np.ones((3,3),np.uint8)
+    # clean_img = cv.morphologyEx(binary_img, cv.MORPH_OPEN, kernel)
+
+    return binary_img
+
+
+def find_score(arr, angle):
+    data = inter.rotate(arr, angle, reshape=False, order=0)
+    hist = np.sum(data, axis=1)
+    score = np.sum((hist[1:] - hist[:-1]) ** 2)
+    return hist, score
+
+
+def deskew(binary_img):
+    ht, wd = binary_img.shape
+    # _, binary_img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+    # pix = np.array(img.convert('1').getdata(), np.uint8)
+    bin_img = (binary_img // 255.0)
+
+    delta = 0.1
+    limit = 3
+    angles = np.arange(-limit, limit + delta, delta)
+    scores = []
+    for angle in angles:
+        hist, score = find_score(bin_img, angle)
+        scores.append(score)
+
+    best_score = max(scores)
+    best_angle = angles[scores.index(best_score)]
+    # print('Best angle: {}'.formate(best_angle))
+
+    # correct skew
+    data = inter.rotate(bin_img, best_angle, reshape=False, order=0)
+    img = im.fromarray((255 * data).astype("uint8"))
+
+    # img.save('skew_corrected.png')
+    pix = np.array(img)
+    return pix
+
+
+def save_image(img, folder, title):
+    cv.imwrite(f'{folder}/{title}.png', img)
+
+
+
+def read_image(img, folder, title):
+    image = cv.imread(f'{folder}/{title}.png')
+    return image
+
+def projection(gray_img, axis: str = 'horizontal'):
+    """ Compute the horizontal or the vertical projection of a gray image """
+
+    if axis == 'horizontal':
+        projection_bins = np.sum(gray_img, 1).astype('int32')
+    elif axis == 'vertical':
+        projection_bins = np.sum(gray_img, 0).astype('int32')
+
+    return projection_bins
+
+
+def preprocess(image):
+    # Maybe we end up using only gray level image.
+    gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray_img = cv.bitwise_not(gray_img)
+
+    binary_img = binary_otsus(gray_img, 0)
+    # cv.imwrite('origin.png', gray_img)
+
+    # deskewed_img = deskew(binary_img)
+    deskewed_img = deskew(binary_img)
+    # cv.imwrite('output.png', deskewed_img)
+
+    # binary_img = binary_otsus(deskewed_img, 0)
+    # breakpoint()
+
+    # Visualize
+
+    # breakpoint()
+    return deskewed_img
+
+
+def projection_segmentation(clean_img, axis, cut=3):
+    segments = []
+    start = -1
+    cnt = 0
+
+    projection_bins = projection(clean_img, axis)
+    for idx, projection_bin in enumerate(projection_bins):
+
+        if projection_bin != 0:
+            cnt = 0
+        if projection_bin != 0 and start == -1:
+            start = idx
+        if projection_bin == 0 and start != -1:
+            cnt += 1
+            if cnt >= cut:
+                if axis == 'horizontal':
+                    segments.append(clean_img[max(start - 1, 0):idx, :])
+                elif axis == 'vertical':
+                    segments.append(clean_img[:, max(start - 1, 0):idx])
+                cnt = 0
+                start = -1
+
+    return segments
+
+
+def line_horizontal_projection(image, cut=3):
+    # Preprocess input image
+    clean_img = preprocess(image)
+
+    # Segmentation
+    lines = projection_segmentation(clean_img, axis='horizontal', cut=cut)
+
+    return lines
+
+
+def word_vertical_projection(line_image, cut=3):
+    line_words = projection_segmentation(line_image, axis='vertical', cut=cut)
+    line_words.reverse()
+
+    return line_words
+
+
+def extract_words(img, visual=0):
+    lines = line_horizontal_projection(img)
+    words = []
+
+    for idx, line in enumerate(lines):
+
+        if visual:
+            save_image(line, LINES_UPLOAD_FOLDER, f'line{idx}')
+
+    #     line_words = word_vertical_projection(line)
+    #     for w in line_words:
+    #         # if len(words) == 585:
+    #         #     print(idx)
+    #         words.append((w, line))
+    #     # words.extend(line_words)
+    #
+    # # breakpoint()
+    # if visual:
+    #     for idx, word in enumerate(words):
+    #         save_image(word[0], WORDS_UPLOAD_FOLDER, f'word{idx}')
+
+    return lines
+
+
 @app.route('/')
 def home():
     return "Hello World"
@@ -52,24 +230,35 @@ def predict():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config['WORDS_UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config['LINES_UPLOAD_FOLDER'], filename))
         print('upload_image filename: ' + app.config['UPLOAD_FOLDER'] + filename)
         img_path = app.config['UPLOAD_FOLDER'] + filename
         image = cv.imread(img_path)
+
+        # segmentation
+        lines = extract_words(image, 1)
+
         # Loading Model
         tf.compat.v1.disable_eager_execution()
         sess = tf.compat.v1.Session()
         model = tf.compat.v1.saved_model.loader.load(sess, tags=['serve'],
                                                      export_dir='model_pb')
-        # Get Predicted Text
-        resized_image = tf.compat.v1.image.resize_image_with_pad(image, 64, 1024).eval(session=sess)
-        img_gray = cv.cvtColor(resized_image, cv.COLOR_RGB2GRAY).reshape(64, 1024, 1)
+        output_text = ""
+        for idx, line in enumerate(lines):
+            image = read_image(line, LINES_UPLOAD_FOLDER, f'line{idx}')
+            # Get Predicted Text
+            resized_image = tf.compat.v1.image.resize_image_with_pad(image, 64, 1024).eval(session=sess)
+            img_gray = cv.cvtColor(resized_image, cv.COLOR_RGB2GRAY).reshape(64, 1024, 1)
 
-        output = sess.run('Dense-Decoded/SparseToDense:0',
-                          feed_dict={
-                              'Deep-CNN/Placeholder:0': img_gray
-                          })
-        output_text = dense_to_text(output[0])
-        output_text = {'output_text' : output_text}
+            output = sess.run('Dense-Decoded/SparseToDense:0',
+                              feed_dict={
+                                  'Deep-CNN/Placeholder:0': img_gray
+                              })
+            out = dense_to_text(output[0])
+            output_text += out
+
+        output_text = {'output_text': output_text}
         return jsonify(output_text)
     else:
         return "Allowed image types are - png, jpg, jpeg, gif"
